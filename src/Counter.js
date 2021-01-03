85901103d74cc5e6d9a8d7a079ec82ee4143f120
kenzo <kenzou.kenzou104809@gmail.com>
2021-01-03 20:24:13 +0900

    feat: setCountに関数を渡す形式を追加

--- src/Counter.js
+++ src/Counter.js
@@ -7,6 +7,10 @@ const Counter = () => {
     <>
       <p>Count: {count}</p>
       <button onClick={() => setCount(count + 1)}>Add</button>
+      {/* setCountに関数を渡す形 */}
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Add with function.
+      </button>
     </>
   );
 };
