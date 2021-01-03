e8c51b933ed4c3a8d31b6303e2c7b10ed291732b
kenzo <kenzou.kenzou104809@gmail.com>
2021-01-03 20:27:44 +0900

    feat: useEffectを使った処理のサンプルを追加

--- src/Counter.js
+++ src/Counter.js
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+
+  // 副作用はコンポーネントの描画後に実行されるので、下記のDOMの操作もできる
+  // コンポーネントは state が更新されるたびに再描画されるので、下記処理も count が切り替わると実行される
+  useEffect(() => {
+    document.getElementById("effect").innerHTML = `Count is ${count}`;
+  });
 
   return (
     <>
@@ -11,6 +17,7 @@ const Counter = () => {
       <button onClick={() => setCount((prevCount) => prevCount + 1)}>
         Add with function.
       </button>
+      <p id="effect"></p>
     </>
   );
 };
